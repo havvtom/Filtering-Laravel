4278ed28ee3ace81ea7eefee49e35b818fa8f668
Tom Havatyi <havvtom@gmail.com>
2019-11-18 08:43:49 +0200

    adding more filters

--- app/Filters/Course/AccessFilter.php
+++ app/Filters/Course/AccessFilter.php
@@ -2,14 +2,29 @@
 
 namespace App\Filters\Course;
 use Illuminate\Database\Eloquent\Builder;
+use App\Filters\FilterAbstract;
 
-class AccessFilter{
+class AccessFilter extends FilterAbstract {
 
-	public function filter(Builder $builder, $value){
+	public function mappings(){
 
-		$value = ($value === 'free' ? true : false);
+		return [
+			'free' => true,
+			'premium' => false
+		];
+	}
 
+
+	public function filter(Builder $builder, $key){
+
+		$value = $this->resolveFilterValue($key);
+	
+		if($value === null){
+			return $builder;
+		}
 		return $builder->where('free', $value);
 	}
+
+	
 	
 }
--- app/Filters/Course/DifficultyFilter.php
+++ app/Filters/Course/DifficultyFilter.php
@@ -1,6 +1,15 @@
 <?php
 namespace App\Filters\Course;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filters\FilterAbstract;
 
-class DifficultyFilter{
+class DifficultyFilter extends FilterAbstract{
+
+	public function filter(Builder $builder, $value){
+
+		// $value = ($value === 'free' ? true : false);
+
+		echo $value;
+	}
 
 }
